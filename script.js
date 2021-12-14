d002ae4588d08e5b00bab3e0c606f2891199fefa
shayestehfar <m.shayestehfar@gmail.com>
2021-12-14 11:00:04 +1100

    Final

--- script.js
+++ script.js
@@ -4,11 +4,10 @@ btnEl.addEventListener('click', function (e) {
   // claculate the top and left positions of span based on clicked point and the button location
   const x = e.clientX // left position of clicked point
   const y = e.clientY // top position of clicked point
-  const xOffset = e.target.offsetLeft // left corner of target element (button)
-  const yOffset = e.target.offsetTop // top corner of target element (button)
+  const xOffset = btnEl.offsetLeft // left corner of target element (button)
+  const yOffset = btnEl.offsetTop // top corner of target element (button)
   const circleLeft = x - xOffset
   const circleTop = y - yOffset
-
   // create span element
   const circle = document.createElement('span')
   circle.classList.add('circle')
